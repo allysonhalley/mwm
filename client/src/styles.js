aed6f0e0d86088ae76b2446b9ec4767960c63ee1
Allyson Halley <allysonhalley@gmail.com>
2021-10-26 23:08:33 -0300

    initial struture

---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(175, 13, 13, 0.911)',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
