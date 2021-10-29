a535496c6df1bc939abb81c61e94270b801803f6
Allyson Halley <allysonhalley@gmail.com>
2021-10-29 00:06:04 -0300

    login in progress

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 app.use('/', (req, res) => {
   res.send('Welcome Enophile to MWM!');
